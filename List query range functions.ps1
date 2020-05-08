2e63dd6428e6489e3165af907f717a791e1c02b3
Dag Calafell, III <32473582+dodiggitydag@users.noreply.github.com>
2020-05-08 00:32:45 -0500

    Add files via upload

---
+++ List query range functions.ps1
@@ -0,0 +1,18 @@
+﻿$rootDirectory = "C:\AOSService\PackagesLocalDirectory"
+
+cd $rootDirectory
+ls "*.xml" -Recurse | % {
+    $matches = (gc $_.FullName -ReadCount 0 | Select-String '\[QueryRangeFunction(?:Attribute)?\(?\)?\]\s+?public static ([^\r\n ]+) ([^\r\n)]+)' -AllMatches).Matches
+
+    if ($matches.Count -ne 0)
+    {
+        $file = $_.Name.Substring(0, $_.Name.Length -4)
+        #$fullName = $_.FullName
+
+        foreach ($m in $matches) {
+            Write-Host "$($m.Groups[1]) " -NoNewline
+            Write-Host "$($file)." -NoNewline -ForegroundColor Gray
+            Write-Host "$($m.Groups[2]))"
+        }
+    }
+}
